c279d145e6c864610ae51041924a0cdbda676571
elissssgi <114120939+elissssgi@users.noreply.github.com>
2024-11-11 08:04:05 -0500

    a

--- final-distribuidos-proyecto/GestionarUsuario.aspx.vb
+++ final-distribuidos-proyecto/GestionarUsuario.aspx.vb
@@ -1,8 +1,2 @@
-﻿Public Class GestionarUsuario
-    Inherits System.Web.UI.Page
+﻿
 
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
-    End Sub
-
-End Class
